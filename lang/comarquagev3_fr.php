af639a0d7eb56a4e3f4ee068aee90554609775c5
Laurent Vergerolle <laurent@ipeos.com>
2017-01-16 10:33:52 -0400

    fichier de langue utilisant la balise comarquagev3

---
+++ lang/comarquagev3_fr.php
@@ -0,0 +1,58 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    #A
+    'acceder_service_ligne' => 'Accéder au service en ligne',
+    'actualites' => 'Actualités',
+    'aider_remplir_form' => 'Pour vous aider à remplir le formulaire :',
+    'aussi_utiliser' => 'Vous pouvez aussi utiliser :',
+
+    #C
+    'centredecontact' => 'Centre de contact',
+    'cerfa' => 'Cerfa n°',
+    'commentfairesi' => 'Comment faire si...',
+
+    #E
+    'explication_pratiques' => 'Pour toute explication, consulter les fiches pratiques :',
+
+    #D
+    'dernieres_actualites' => 'Dernières actualités',
+
+    #F
+    'ficheliees' => 'Fiches associées',
+
+    #O
+    'ousadresser' => 'Où s\'informer ?',
+
+    #Q
+    'questionreponses' => 'Questions ? Réponses !',
+
+    #P
+    'page_404_texte' => '<p>Désolé, cette page n\'est pas disponible sur
+	service-public.fr</p>
+	<p>Vous pouvez toutefois <a href="#" onclick="window.history.back();">
+	consulter les autres pages</a></p>',
+    'page_404_titre' => 'Page indisponible',
+    'pour_en_savoir_plus' => 'Pour en savoir plus',
+    'prive_visible_public' => 'Les informations de comarquage ne sont pas chargé
+	dans l\'espace d\'administration.',
+
+    #S
+    'service_ligne' => 'Service en ligne >',
+    'services_en_ligne' => 'Services en ligne et formulaires',
+
+    #R
+    'reference' => 'Textes de référence',
+
+    #T
+    'telecharger_formulaire' => 'Télécharger le formulaire',
+    'titre_accueil' => 'Guide des droits et démarches des @type@',
+    'tous_commentfairesi' => 'Tous les Comment faire si...',
+
+    #V
+    'verifier_le' => 'Vérifié le',
+    'voiraussi' => 'Et aussi sur service-public.fr',
+);
+?>
